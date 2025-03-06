1de61c708958c76cfdd9025792a71d2cb1311f73
Vitor Hugo <vitorfioriv@gmail.com>
2025-03-06 19:48:36 -0300

    Adicionar método olaMundo

--- Hello.java
+++ Hello.java
@@ -6,4 +6,8 @@ public class Hello {
     public static void helloWorld() {
         System.out.println("Hello World");
     }
-} 
+}
+
+	public static void olaMundo() {
+	    System.out.println("Olá Mundo");
+	}
